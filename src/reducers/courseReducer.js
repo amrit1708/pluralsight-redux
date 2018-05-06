f5bd3a60b25ee59a5ef0dd58aa96252791298cb6
LAPTOP-HEMQINEM\Amrit <rsingh@incapsulate.com>
2018-05-06 18:56:15 +0530

    Completed Redux Flow

--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -1,6 +1,9 @@
+import * as types from '../actions/actionTypes';
+
+
 function courseReducer(state=[],action){
     switch(action.type){
-        case 'CREATE_COURSE':
+        case types.CREATE_COURSE:
             return [...state,Object.assign({},action.course)]; //Returns an array with state and the added course
         default : 
             return state;
