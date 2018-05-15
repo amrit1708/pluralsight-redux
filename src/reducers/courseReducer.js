38dcbfef755892b0439caac81ab16e8cd9969326
LAPTOP-HEMQINEM\Amrit <rsingh@incapsulate.com>
2018-05-15 12:56:08 +0530

    Completed Async Writes in Redux

--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -1,13 +1,25 @@
 import * as types from '../actions/actionTypes';
+import initialState from './initialState';
 
-
-function courseReducer(state=[],action){
+function courseReducer(state=initialState.courses,action){
     switch(action.type){
         case types.LOAD_COURSES_SUCCESS:
-            return action.courses; //Returns an array with state and the added course      
+            return action.courses; //Returns an array with state and the added course 
+        
+        case types.CREATE_COURSE_SUCCESS:
+            return [
+                ...state,
+                Object.assign({},action.course)
+            ];
+        
+        case types.UPDATE_COURSE_SUCCESS:
+            return [
+                ...state.filter(course => course.id !== action.course.id),
+                Object.assign({},action.course)
+            ];
+            
         default : 
             return state;
-        
     }
 }
 
